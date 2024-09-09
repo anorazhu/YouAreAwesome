ccfc67978f5a1690175c719d2e513d41f9d71e22
anorazhu <zhusm@bc.edu>
2024-09-09 17:56:02 -0400

    testing

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
+            
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
